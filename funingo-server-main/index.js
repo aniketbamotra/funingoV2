8481848d5c8d3a5165062d3d589a6d80f1cc1ba7
Prateek Goyal <gprateek015@gmail.com>
2024-10-29 13:01:29 +0530

    testing cors issue

--- funingo-server-main/index.js
+++ funingo-server-main/index.js
@@ -63,9 +63,9 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: ["*"],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    origin: "*",
+    methods: ["*"],
+    allowedHeaders: ["*"],
     credentials: true,
   })
 );
